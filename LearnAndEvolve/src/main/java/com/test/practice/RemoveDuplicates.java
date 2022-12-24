9163c8aeab8efafa1dd9839b3f001990cca48ecc
Abhi4ever123 <abhiforever12345@gmail.com>
2022-12-24 23:11:57 +0530

    updated RemoveDuplicate class file

--- LearnAndEvolve/src/main/java/com/test/practice/RemoveDuplicates.java
+++ LearnAndEvolve/src/main/java/com/test/practice/RemoveDuplicates.java
@@ -1,6 +1,8 @@
 package com.test.practice;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class RemoveDuplicates {
 
@@ -45,7 +47,28 @@ public class RemoveDuplicates {
 					sb.append(arr[i]);
 				}
 			}
+			
+			System.out.println(sb);
 		}
+	}
+	
+	
+	public static void usingSetInterface(String str)
+	{
+		StringBuilder sb = new StringBuilder();
+		Set<Character> set = new LinkedHashSet<>();
+		for(int i=0; i< str.length(); i++)
+		{
+			set.add(str.charAt(i));
+		}
+		
+		for(Character c: set)
+		{
+			sb.append(c);
+		
+		}
+		
+		System.out.println(sb);
 	}
 	
 	
